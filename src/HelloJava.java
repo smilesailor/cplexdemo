47dcde68267d261a9131261b6f16b869618db4ea
Lenovo <li_hong_ming@163.com>
2020-06-15 17:37:37 +0800

    Hello Homing

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -2,5 +2,6 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Hello Cplex");
+        System.out.println("Hello Homing");
     }
 }
